25859f655745d00eb414c4edd29b51f22bc731dc
Phuoc Nhat <doanphuocnhat205@gmail.com>
2022-05-05 13:33:59 +0700

    Typing code

---
+++ Music Player in JavaScript/js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "An Thần (ft. Thắng) | Low G",
+    artist: "Thắng & Chí Trung",
+    img: "music-1",
+    src: "AnThan"
+  },
+  {
+    name: "SƠN TÙNG M-TP | THERE'S NO ONE AT ALL",
+    artist: "SƠN TÙNG M-TP",
+    img: "4k",
+    src: "THERESNOONEATALL"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
